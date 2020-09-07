abef90a4660f84a15b05361191fbfe58a91c4590
ReHaugen <Reidar_Haugen@outlook.com>
2020-09-07 14:00:14 +0200

    Test one fail

--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -6,10 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
 
-    @Test
-    void shouldScoreOnes(){
-        assertEquals(4, new YatzyGame() .score(YatzyCategory.ONES, new int[] {1, 1, 2, 1, 1}));
-    }
+  @Test
+    void shouldScoreOnes () {
+      assertEquals(4, new YatzyGame() .score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1}));
+  }
 }
 
-//hallo
