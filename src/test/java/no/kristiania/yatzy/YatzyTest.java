00f642fa49bfa0c07561ea5feceaa421f537ac97
ReHaugen <Reidar_Haugen@outlook.com>
2020-09-07 13:09:30 +0200

    Test en feilet

--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
     @Test
-    void shouldScoreOnes(){
-        assertEquals(4, new YatzyGame() .score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1}));
+    void shouldScoreOne() {
+        assertEquals(4 , new YatzyGame() .score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1}));
     }
+
 }
-//Hello
