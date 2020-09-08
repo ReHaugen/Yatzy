c9372e4c59ea363d30f421d74d8b3e559ad87e68
ReHaugen <Reidar_Haugen@outlook.com>
2020-09-08 10:10:54 +0200

    Delete files

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -1,14 +1,6 @@
 package no.kristiania.yatzy;
 
 public class YatzyGame {
-    public int score(YatzyCategory ones, int[] dice) {
-        int result = 0;
-            for (int die : dice) {
-                if (die == 1) {
-                    result += 1;
-                }
-            }
-            return result;
-        }
-    }
+
+}
 
--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -5,19 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
-
-  private YatzyGame yatzyGame = new YatzyGame();
-
-  @Test
-    void shouldScoreOnes () {
-      assertEquals(4, yatzyGame .score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1}));
-      assertEquals(2, yatzyGame .score(YatzyCategory.ONES, new int[] { 6, 5, 2, 1, 1}));
-  }
-
-    @Test
-    void shouldScoreOnes () {
-        assertEquals(4, yatzyGame .score(YatzyCategory.TWOS, new int[] { 1, 1, 2, 1, 1}));
-        assertEquals(2, yatzyGame .score(YatzyCategory.TWOS, new int[] { 6, 5, 2, 1, 1}));
-    }
+    return 0;
 }
 
