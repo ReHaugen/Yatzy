49e75bbe613c8e6e1b02c2a78013ae85a0a16e14
ReHaugen <Reidar_Haugen@outlook.com>
2020-09-08 10:45:26 +0200

    Test en failer

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -1,6 +1,11 @@
 package no.kristiania.yatzy;
 
+
 public class YatzyGame {
 
+    public int score(YatzyCategory ones, int[] dice) {
+        return 0;
+
+    }
 }
 
--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
-    return 0;
+
+    @Test
+    void shouldScoreOnes(){
+        assertEquals(4, new YatzyGame().score(YatzyCategory.ONES, new int[] {1, 1, 2, 1, 1}));
+    }
+
 }
 
